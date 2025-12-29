48daee48437bc340e9e2ec6939dcddc03024979a
sghadir2@gmail.com <Ghadir>
2025-12-29 18:50:40 +0330

    update

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "glass-bg": "rgba(9, 21, 42, 0.6)",
+        "glass-border": "#1A3150",
+        "card-overlay": "rgba(21, 32, 55, 0.9)",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+};

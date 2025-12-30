12a6bae9e35a6cccefc0798f2693f32345546127
sghadir2@gmail.com <Ghadir>
2025-12-30 19:41:19 +0330

    updated lobby page design

--- src/app/lobby/page.tsx
+++ src/app/lobby/page.tsx
@@ -12,25 +12,26 @@ function LobbyPage() {
 
   const gameCards: GameTypeEnum[] = [0, 1, 2];
 
-  const onClickPlayHandle = () => {
+  const onClickPlayHandle = (idx: number) => {
+    setGameType(idx);
     openModal("game");
   };
 
   return (
     <main className="flex-1 w-full px-6 flex gap-4 items-center justify-center mt-2">
       {gameCards.map((game) => (
-        <div className="game-card relative group w-1/3 h-[140px] rounded-xl overflow-hidden cursor-pointer shadow-lg border border-transparent hover:border-[#1A3150]">
+        <div className="game-card relative group w-1/3 h-1/2   rounded-xl overflow-hidden cursor-pointer shadow-lg border border-transparent hover:border-[#1A3150]">
           <img
-            src={`images/lobby/${GameTypeEnum[game]}.png`}
+            src={`images/lobby/${GameTypeEnum[game].toLowerCase()}.png`}
             className="w-full h-full object-cover"
             alt="Backgammon Board"
           />
           <div
-            onClick={onClickPlayHandle}
+            onClick={() => onClickPlayHandle(game)}
             className="absolute bottom-0 w-full h-[36px] bg-card-overlay backdrop-blur-sm flex justify-between items-center px-3 border-t border-white/10"
           >
             <span className="text-white text-xs font-light">
-              Play Now! - SINGLE
+              {GameTypeEnum[game]}
             </span>
             <ChevronRight className="fas fa-chevron-right text-gray-400 text-xs group-hover:text-white transition-colors" />
           </div>
--- src/components/modals/gameModal.tsx
+++ src/components/modals/gameModal.tsx
@@ -4,23 +4,45 @@ import React, { useState } from "react";
 import { X, Crown, Gem } from "lucide-react";
 import { useModalStore } from "@/store/useModalStore.tsx";
 import { GameTypeEnum } from "@/types/lobby.ts";
+import Input from "@/components/Input.tsx";
+import { Button } from "@/components";
+
+interface PrivateGame {
+  amount: string;
+  telegramId: string;
+}
 
 export default function GameModal() {
   // State to track which slot is selected (optional interactivity)
   const [selectedSlot, setSelectedSlot] = useState<number | null>(1);
+  const [privateGame, setPrivateGame] = useState<PrivateGame>({
+    amount: "",
+    telegramId: "",
+  });
   const betOptions = [0.5, 1, 3];
 
   const closeModal = useModalStore((state) => state.closeModal);
   const gameType = useModalStore((state) => state.gameType);
 
-  const onClickPlayHandle = () => {
-    closeModal();
+  const onBetAmountChangeHandle = (e) => {
+    setPrivateGame((prevState) => ({
+      ...prevState,
+      amount: e.target.value,
+    }));
   };
+
+  const onTelegramIdChangeHandle = (e) => {
+    setPrivateGame((prevState) => ({
+      ...prevState,
+      telegramId: e.target.value,
+    }));
+  };
+
   return (
     // Backdrop Overlay
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm p-4">
       {/* Modal Container */}
-      <div className="relative w-full max-w-[560px] overflow-hidden rounded-2xl border border-[#1A3150] bg-[#09152A]/85 shadow-2xl backdrop-blur-xl">
+      <div className="relative w-full min-h[400px] max-w-[560px] overflow-hidden rounded-2xl border border-[#1A3150] bg-[#09152A]/85 shadow-2xl backdrop-blur-xl ">
         {/* Header Section */}
         <div className="flex items-center justify-between border-b border-[#1A3150] px-6 py-5">
           <h2 className="text-2xl font-normal uppercase tracking-wide text-white">
@@ -38,12 +60,13 @@ export default function GameModal() {
 
         {/* Betting Options Row */}
         {gameType == GameTypeEnum.ONLINE && (
-          <div className="flex flex-wrap justify-center gap-5 mb-10">
-            {betOptions.map((amount) => (
-              <button
-                key={amount}
-                onClick={() => setSelectedSlot(amount)}
-                className={`
+          <div className="flex flex-col items-center py-12">
+            <div className="flex gap-4 mb-10">
+              {betOptions.map((amount) => (
+                <button
+                  key={amount}
+                  onClick={() => setSelectedSlot(amount)}
+                  className={`
                   relative flex h-[60px] min-w-[100px] items-center justify-center gap-3 rounded-xl border transition-all duration-200 px-4
                   ${
                     selectedSlot === amount
@@ -51,19 +74,31 @@ export default function GameModal() {
                       : "border-[#1A3150] bg-[#09152A]/40 hover:border-[#2a4a75] hover:bg-[#1A3150]/60" // Default
                   }
                 `}
-              >
-                {/* TON Icon Representation */}
-                <div className="flex h-6 w-6 items-center justify-center rounded-full bg-[#0098EA] shadow-sm">
-                  <Gem
-                    size={12}
-                    className="text-white fill-white translate-y-[1px]"
-                  />
-                </div>
+                >
+                  {/* TON Icon Representation */}
+                  <div className="flex h-6 w-6 items-center justify-center rounded-full bg-[#0098EA] shadow-sm">
+                    <Gem
+                      size={12}
+                      className="text-white fill-white translate-y-[1px]"
+                    />
+                  </div>
 
-                {/* Amount Text */}
-                <span className="text-lg font-medium text-white">{amount}</span>
-              </button>
-            ))}
+                  {/* Amount Text */}
+                  <span className="text-lg font-medium text-white">
+                    {amount}
+                  </span>
+                </button>
+              ))}
+            </div>
+            <Button
+              label={"CONTINUE"}
+              icon={
+                <Crown
+                  size={10}
+                  className="text-white fill-white translate-y-[0.5px]"
+                />
+              }
+            />
           </div>
         )}
 
@@ -99,14 +134,33 @@ export default function GameModal() {
             </div>
 
             {/* Continue Button */}
-            <button className="group flex h-12 min-w-[160px] items-center justify-center gap-3 rounded-xl border border-[#1A3150] bg-[#09152A]/60 px-8 transition-all hover:bg-[#1A3150] hover:shadow-lg active:scale-95">
-              <div className="flex h-5 w-5 items-center justify-center rounded-full bg-gradient-to-b from-yellow-400 to-yellow-600">
-                <Crown size={10} className="text-white fill-white" />
-              </div>
-              <span className="text-sm font-medium uppercase tracking-wide text-white">
-                Continue
-              </span>
-            </button>
+            <Button
+              label={"LAUNCH"}
+              icon={
+                <Gem
+                  size={10}
+                  className="text-white fill-white translate-y-[0.5px]"
+                />
+              }
+            />
+          </div>
+        )}
+        {gameType == GameTypeEnum.PRIVATE && (
+          <div className="flex flex-col items-center py-12 gap-5">
+            <Input
+              value={privateGame.amount}
+              onChange={onBetAmountChangeHandle}
+              onSubmit={() => null}
+              placeholder={"Enter amount of TON..."}
+              isLoading={false}
+            />
+            <Input
+              value={privateGame.telegramId}
+              onChange={onBetAmountChangeHandle}
+              onSubmit={() => null}
+              placeholder={"Enter Telegram ID..."}
+              isLoading={false}
+            />
           </div>
         )}
       </div>

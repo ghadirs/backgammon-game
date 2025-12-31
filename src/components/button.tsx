e4f57d70d57ed3b056bceebade7f71dac9ff04e2
Ghadir github <ghadirshamohamadi@yahoo.com>
2025-12-31 23:56:36 +0330

    removed residual style files, fixed the layout for gameplay page, fixed the functionality for quit.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,31 +1,34 @@
 import React from "react";
 
 interface ButtonProps {
-  icon?: React.ReactNode;
-  label: string;
-  onClick?: () => void;
-  className?: string;
+    icon?: React.ReactNode;
+    label: string;
+    onClick?: () => void;
+    className?: string;
+    children?: React.ReactNode;
 }
 
 export default function Button({
-  icon,
-  label,
-  onClick,
-  className,
-}: ButtonProps) {
-  return (
-    <button
-      onClick={onClick}
-      className={
-        className
-          ? className
-          : `group flex h-12 min-w-[160px] items-center justify-center gap-3 rounded-xl border border-[#1A3150] bg-[#09152A]/60 px-8 transition-all hover:bg-[#1A3150] hover:shadow-lg active:scale-95 cursor-pointer`
-      }
-    >
-      {icon}
-      <span className="text-sm font-medium uppercase tracking-wide text-white">
-        {label}
-      </span>
-    </button>
-  );
+                                   icon,
+                                   label,
+                                   onClick,
+                                   className,
+                                   children
+                               }: ButtonProps) {
+    return (
+        <button
+            onClick={onClick}
+            className={
+                className
+                    ? className
+                    : `group flex h-12 min-w-[160px] items-center justify-center gap-3 rounded-xl border border-[#1A3150] bg-[#09152A]/60 px-8 transition-all hover:bg-[#1A3150] hover:shadow-lg active:scale-95 cursor-pointer`
+            }
+        >
+            {!children ? <>{icon}
+                <span className="text-sm font-medium uppercase tracking-wide text-white">
+      {label}
+    </span> </> : children
+            }
+        </button>
+    );
 }
--- src/components/toggle.tsx
+++ src/components/toggle.tsx
@@ -3,13 +3,14 @@ import styles from './toggle.module.scss';
 
 const Toggle = ({enabled, setEnabled}: { enabled: boolean, setEnabled: (v: boolean) => void }) => {
     return (
-        <div className={styles.wrapper}>
-            <span className={styles.label}>Auto roll</span>
+        <div className="flex flex-col gap-3 ">
+            <span className="text-white text-base text-xs">Auto roll</span>
             <button
-                className={`${styles.switch} ${enabled ? styles.active : ''}`}
                 onClick={() => setEnabled(!enabled)}
+                className={`relative w-[50px] h-[26px] rounded-[4px] cursor-pointer transition-colors ${enabled ? 'bg-[#3182ce]' : 'bg-[#1c2638]'}`}
             >
-                <div className={styles.handle}/>
+                <div
+                    className={`absolute top-[2px] left-[2px] w-[22px] h-[22px] bg-white rounded-[3px] transition-transform duration-200 ease-out ${enabled ? 'translate-x-[24px]' : 'translate-x-0'}`}/>
             </button>
         </div>
     );

322ec7f54d454a8aefed91f8b514983949b49017
sghadir2@gmail.com <Ghadir>
2026-01-06 10:22:56 +0330

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    sassOptions: {
-        includePaths: ['./src/app'],
-    },
-    images: {unoptimized: true},
+  sassOptions: {
+    includePaths: ["./src/app"],
+  },
+  output: "export", // <--- THIS IS THE KEY LINE
+  basePath: process.env.NODE_ENV === "production" ? "/backgammon-game" : "",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;

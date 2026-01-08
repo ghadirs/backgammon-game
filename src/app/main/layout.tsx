34d95b0d5d49601223055d5cac653f44a6cd6ec7
sghadir2@gmail.com <Ghadir>
2026-01-08 11:17:22 +0330

    feat(layout): add bottom navbar to main layout
    The BottomNavbar component was added to provide navigation functionality at the bottom of the main layout. This improves mobile user experience by making navigation more accessible.

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,28 +1,38 @@
-"use client"
+"use client";
 
-import {X} from "lucide-react";
+import { X } from "lucide-react";
 import React from "react";
 import Link from "next/link";
-import {usePathname} from "next/navigation";
+import { usePathname } from "next/navigation";
+import BottomNavbar from "@/components/bottomNavbar";
 
-export default function MainLayout({children}: { children: React.ReactNode }) {
-    const path = usePathname();
-    return (
-        <>
-            <div className="flex items-center justify-end border-b border-[#1A3150] py-1.5 px-2 ">
-                <h2 className="text-2xl font-normal uppercase ml-auto mr-auto tracking-wide text-white">
-                    {path == "/main/wallet" ? "WALLET" : path == "/main/leaders" ? "LEADERS" : "FRIENDS"}
-                </h2>
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const path = usePathname();
+  return (
+    <>
+      <div className='flex items-center justify-end border-b border-[#1A3150] py-1.5 px-2 '>
+        <h2 className='text-2xl font-normal uppercase ml-auto mr-auto tracking-wide text-white'>
+          {path == "/main/wallet"
+            ? "WALLET"
+            : path == "/main/leaders"
+            ? "LEADERS"
+            : "FRIENDS"}
+        </h2>
 
-                {/* Close Button */}
-                <Link
-                    href='/lobby'
-                    className="flex h-9 w-9 items-center justify-center rounded-lg border border-red-900/50 bg-[#FF2428] text-white shadow-lg transition-transform hover:scale-105 active:scale-95 cursor-pointer"
-                >
-                    <X size={20} strokeWidth={3}/>
-                </Link>
-            </div>
-            {children}
-        </>
-    )
+        {/* Close Button */}
+        <Link
+          href='/lobby'
+          className='flex h-9 w-9 items-center justify-center rounded-lg border border-red-900/50 bg-[#FF2428] text-white shadow-lg transition-transform hover:scale-105 active:scale-95 cursor-pointer'
+        >
+          <X size={20} strokeWidth={3} />
+        </Link>
+      </div>
+      {children}
+      <BottomNavbar />
+    </>
+  );
 }
